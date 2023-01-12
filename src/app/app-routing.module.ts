81a1eae28ff045c5071b1a906b3a824ea73e6927
M-Asad-Khan <asad.khan@devbox.co>
2023-01-12 19:42:50 +0500

    apply auth guard on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,21 +10,22 @@ import { SellerNotificationsComponent } from 'src/components/pages/seller/seller
 import { SECDisclosureComponent } from 'src/components/pages/informations/sec-disclosure/sec-disclosure.component';
 import { UserAgreementComponent } from 'src/components/pages/informations/user-agreement/user-agreement.component';
 import { MyListingComponent } from 'src/components/pages/seller/my-listing/my-listing.component';
+import { AuthGuard } from 'src/components/auth/auth.guard';
 
 
 
 const routes: Routes = [
 
   { path: "", component: LoginComponent },
-  { path: "market-place", component: MarketPlaceComponent },
-  { path: "my-bids", component: MyBidsComponent },
-  { path: "buyer-transactions", component: BuyerTransactionsComponent },
-  { path: "buyer-notifications", component: BuyerNotificationsComponent },
-  { path: "my-listing", component: MyListingComponent },
-  { path: "seller-transactions", component: SellerTransactionsComponent },
-  { path: "seller-notifications", component: SellerNotificationsComponent },
-  { path: "sec-disclosure", component: SECDisclosureComponent },
-  { path: "user-agreement", component: UserAgreementComponent },
+  { path: "market-place", component: MarketPlaceComponent, canActivate: [AuthGuard] },
+  { path: "my-bids", component: MyBidsComponent, canActivate: [AuthGuard] },
+  { path: "buyer-transactions", component: BuyerTransactionsComponent, canActivate: [AuthGuard] },
+  { path: "buyer-notifications", component: BuyerNotificationsComponent, canActivate: [AuthGuard] },
+  { path: "my-listing", component: MyListingComponent, canActivate: [AuthGuard] },
+  { path: "seller-transactions", component: SellerTransactionsComponent, canActivate: [AuthGuard] },
+  { path: "seller-notifications", component: SellerNotificationsComponent, canActivate: [AuthGuard] },
+  { path: "sec-disclosure", component: SECDisclosureComponent, canActivate: [AuthGuard] },
+  { path: "user-agreement", component: UserAgreementComponent, canActivate: [AuthGuard] },
   { path: "**", redirectTo: "", pathMatch: "full" }
 
 ];
--- src/components/auth/login/login.service.ts
+++ src/components/auth/login/login.service.ts
@@ -11,19 +11,29 @@ import { Router } from '@angular/router';
 
 export class LoginService {
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+    this.isAuthanticated = localStorage.getItem("isAuthanticated") == 'true' ? true : false;
+  }
 
   isAuthanticated: boolean = false;
 
+
   login(user: LoginForm): Observable<Login> {
     const res = this.http.post<Login>(`http://52.24.239.223/:8000/auth/login/`, user)
+    localStorage.setItem("isAuthanticated", "true");
+
     this.isAuthanticated = true;
     this.router.navigate(['/market-place'])
     return res;
   }
 
-  logout(){
-    
+  logout() {
+    this.isAuthanticated = false;
+    localStorage.setItem("isAuthanticated", "false");
+
+    this.router.navigate([''])
+
+
   }
 
 }

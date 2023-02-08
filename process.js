46bff7c851629b0a0f29e384d71886d31d58fc87
Ali Hasnain <ali.hasnain@adeccogroup.com>
2023-02-08 13:07:58 +0500

    build

---
+++ process.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    name: 'demoapp',
+    script: 'node_modules/@angular/cli/bin/ng',
+    args: 'serve --host [127.0.0.1] --disable-host-check',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+  }
+};

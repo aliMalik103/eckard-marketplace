8f5adc5ff1c8bb756a41b1ef25a5411a1caecd44
M-Asad-Khan <asad.khan@devbox.co>
2023-02-14 18:18:03 +0500

    remove image cropper package

--- src/components/pages/profile/profile.module.ts
+++ src/components/pages/profile/profile.module.ts
@@ -5,7 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { LoginService } from 'src/components/services/login.service';
 import { ProfileComponent } from './profile.component';
-import { ImageCropperModule } from 'ngx-image-cropper';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
@@ -20,10 +19,8 @@ import { ToastrService } from 'ngx-toastr';
         FormsModule,
         CommonModule,
         BrowserAnimationsModule,
-        ImageCropperModule,
         BrowserModule,
         NgxSpinnerModule
-
     ],
     providers: [LoginService,ToastrService],
     exports: []
